6f5821f4558c27c0b6f7a3119c53d0259f88dc33
KarinaLEOS <kargzz20@gmail.com>
2021-02-28 20:06:14 -0600

    Actividad 5: Simulador.

---
+++ SimuladorDeposito_Act5/SimuladorInversion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SimuladorDeposito_Act5
+{
+   public class SimuladorInversion:INotifyPropertyChanged
+    {
+        private decimal monto;
+        private short plazo;
+        public decimal MontoInvertir
+        {
+
+            get { return monto; }
+            set { monto = value; PropertyChanged(this, new PropertyChangedEventArgs(null)); }
+
+        }
+        public short PlazoAnual
+        {
+
+            get { return plazo; }
+            set { plazo = value; PropertyChanged(this, new PropertyChangedEventArgs(null)); }
+
+        }
+        public decimal Total
+        { 
+            get
+
+            {
+
+                decimal CantidadTotal = monto;
+
+                for (int i = 0; i != plazo; i++)
+
+                {
+
+                    CantidadTotal += (CantidadTotal * 0.07m);
+
+                    CantidadTotal = (CantidadTotal > 30000) ? CantidadTotal - (CantidadTotal * 0.02m) : CantidadTotal;
+
+                }
+                return CantidadTotal;
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
